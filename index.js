3802274679bb8c64b9ff77f5d4dd52ce4dbba314
ri-ri herman <rileyherman@tuta.io>
2020-04-05 16:39:32 -0600

    secrets support

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log(client)
@@ -12,4 +13,4 @@ client.on('message', msg => {
     }
 });
 
-client.login('token');
+client.login(process.env.TOKEN);
